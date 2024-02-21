2442202d374aab2cbd3ac1a9f8e968ca783e92c4
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-02-21 11:21:28 +0100

    add task 01 and task 02

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,61 @@
+"""DB module
+"""
+from sqlalchemy import create_engine , tuple
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    def add_user(self, email: str , hashed_password: str) -> User:
+        """Add a new User in dataBase."""
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self.__session.add(new_user)
+            self.__session.commit()
+        except Exception:
+            self.__session.rollback()
+            new_user=None
+        return new_user
+    def find_user_by(self, **kwargs):
+        """Find user"""
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+
